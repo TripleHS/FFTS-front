2ecae4460e3abe85f3f84dab43124bc5d1f98e37
Sebastian B <sebastian.babinski1@gmail.com>
2021-03-23 21:55:05 +0100

    Configuration in progress

---
+++ client/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const dupa: string = "Olek to mistrzunio";
+console.log(dupa);
+
+ReactDOM.render(<p>{dupa}</p>, document.getElementById("root"));
