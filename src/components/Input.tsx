2e6249dcafce988cfdbde592c797c627d26f7fba
rednaxela700 <ohleg@windowslive.com>
2021-03-24 11:16:51 +0100

    input component cleanup

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
 import handleInputChange from "../utils/handleInputChange";
 
-export default function CreateInput({ type, value, changeState, className }:{type?:string, value?:any, changeState?:Function, className?:string }) {
-  function handleWidth() {
-    if (type === "text") {
-      return "w-2/5";
-    } else {
-      return "w-1/5";
-    }
-  }
+export default function CreateInput({
+  type,
+  value,
+  changeState,
+  className,
+}: {
+  type?: string;
+  value?: any;
+  changeState?: Function;
+  className?: string;
+}) {
+
   return (
     <input
       type={type}
       required
       value={value}
       onChange={(e) => handleInputChange(e, changeState)}
-      className={`${handleWidth()} shadow-md border-gray-500 border ${className}`}
+      className={`${type === 'text'? 'w-2/5':'w-1/5'} shadow-md border-gray-500 border ${className}`}
     />
   );
 }
